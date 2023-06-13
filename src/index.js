baa94fbbe393254ab09058040a82adc05f0f187b
RostyslavMykhats <mirosrik@gmail.com>
2023-06-13 14:11:29 +0300

    header footer

--- src/index.js
+++ src/index.js
@@ -6,35 +6,10 @@ import "./scripts/watchers/_pages"
 // Import utils
 import { initialsBtn } from "./scripts/utils/personalData";
 
+
 // Bisiness logic
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  initialsBtn();
 
-  const PARAMS = {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 2,
-    slidesPerGroup: 2,
-    spaceBetween: 30,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-
-  }
-
-  const swiper = new Swiper('.test_slider', PARAMS);
 });
